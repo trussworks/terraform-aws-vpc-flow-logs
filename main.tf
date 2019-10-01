e786a0e3c5f8539ce154d3c9cb2565d4a5a1618f
Chris Gilmer <chris@truss.works>
2019-10-01 12:43:36 -0700

    Use tf11 code for 1.0.0

--- main.tf
+++ main.tf
@@ -11,19 +11,19 @@
  *
  * ```hcl
  * module "vpc_flow_logs" {
- *   source = "trussworks/vpc-flow-logs/aws"
+ *   source = "./vpc-flow-logs"
  *
- *   vpc_name       = local.vpc_name
- *   vpc_id         = module.vpc.vpc_id
- *   logs_retention = local.cloudwatch_logs_retention
+ *   vpc_name       = "${local.vpc_name}"
+ *   vpc_id         = "${module.vpc.vpc_id}"
+ *   logs_retention = "${local.cloudwatch_logs_retention}"
  * }
  * ```
  */
 
 resource "aws_flow_log" "main" {
-  log_destination = aws_cloudwatch_log_group.main.arn
-  iam_role_arn    = aws_iam_role.main.arn
-  vpc_id          = var.vpc_id
+  log_destination = "${aws_cloudwatch_log_group.main.arn}"
+  iam_role_arn    = "${aws_iam_role.main.arn}"
+  vpc_id          = "${var.vpc_id}"
   traffic_type    = "ALL"
 }
 
@@ -33,7 +33,7 @@ resource "aws_flow_log" "main" {
 
 resource "aws_cloudwatch_log_group" "main" {
   name              = "vpc-flow-logs-${var.vpc_name}"
-  retention_in_days = var.logs_retention
+  retention_in_days = "${var.logs_retention}"
 }
 
 #
@@ -67,11 +67,11 @@ data "aws_iam_policy_document" "role_policy" {
 
 resource "aws_iam_role" "main" {
   name               = "vpc-flow-logs-role-${var.vpc_name}"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
 resource "aws_iam_role_policy" "main" {
   name   = "vpc-flow-logs-role-policy-${var.vpc_name}"
-  role   = aws_iam_role.main.id
-  policy = data.aws_iam_policy_document.role_policy.json
+  role   = "${aws_iam_role.main.id}"
+  policy = "${data.aws_iam_policy_document.role_policy.json}"
 }
